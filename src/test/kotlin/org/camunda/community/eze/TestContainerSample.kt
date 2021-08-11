53b431359acbb9a72c3a7406306089e94cf175b9
Philipp Ossler <philipp.ossler@gmail.com>
2021-08-11 06:36:07 +0200

    test(testcontainer): add more cases

--- src/test/kotlin/org/camunda/community/eze/TestContainerSample.kt
+++ src/test/kotlin/org/camunda/community/eze/TestContainerSample.kt
@@ -1,13 +1,16 @@
 package org.camunda.community.eze
 
 import io.camunda.zeebe.client.ZeebeClient
+import io.camunda.zeebe.client.api.response.DeploymentEvent
 import io.camunda.zeebe.model.bpmn.Bpmn
 import io.zeebe.containers.ZeebeContainer
 import org.assertj.core.api.Assertions.assertThat
+import org.awaitility.kotlin.await
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
+import java.util.concurrent.Future
 
 @Testcontainers
 class TestContainerSample {
@@ -26,23 +29,70 @@ class TestContainerSample {
     }
 
     @Test
+    fun `should deploy process`() {
+        // given
+        val process = Bpmn.createExecutableProcess("process")
+            .startEvent()
+            .endEvent()
+            .done()
+
+        // when
+        val deployFuture: Future<DeploymentEvent> = client.newDeployCommand()
+            .addProcessModel(process, "process.bpmn")
+            .send()
+
+        // then
+        await.untilAsserted {
+            assertThat(deployFuture).isDone
+        }
+    }
+
+    @Test
     fun `should complete process instance`() {
         // given
+        val process = Bpmn.createExecutableProcess("process")
+            .startEvent()
+            .endEvent()
+            .done()
+
         client.newDeployCommand()
-            .addProcessModel(
-                Bpmn.createExecutableProcess("process")
-                    .startEvent()
-                    .serviceTask("task-1").zeebeJobType("test")
-                    .endEvent()
-                    .done(),
-                "process.bpmn"
-            )
+            .addProcessModel(process, "process.bpmn")
+            .send()
+            .join()
+
+        // when
+        val processInstanceResult = client.newCreateInstanceCommand()
+            .bpmnProcessId("process")
+            .latestVersion()
+            .variables(mapOf("x" to 1))
+            .withResult()
+            .send()
+            .join()
+
+        // then
+        assertThat(processInstanceResult.variablesAsMap)
+            .containsEntry("x", 1)
+    }
+
+    @Test
+    fun `should complete process instance with job worker`() {
+        // given
+        val process = Bpmn.createExecutableProcess("process")
+            .startEvent()
+            .serviceTask("task-1").zeebeJobType("test")
+            .endEvent()
+            .done()
+
+        client.newDeployCommand()
+            .addProcessModel(process, "process.bpmn")
             .send()
             .join()
 
         client.newWorker().jobType("test")
             .handler { client, job ->
-                client.newCompleteCommand(job.key).send()
+                client.newCompleteCommand(job.key)
+                    .variables(mapOf("y" to 2))
+                    .send()
             }.open()
 
         // when
@@ -57,6 +107,7 @@ class TestContainerSample {
         // then
         assertThat(processInstanceResult.variablesAsMap)
             .containsEntry("x", 1)
+            .containsEntry("y", 2)
     }
 
 }
